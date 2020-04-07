1aca7ea465ec493bea2c2c2579d2c470b33e4e3a
gequankai <gequankai@zhongye.net>
2020-04-07 15:38:03 +0800

    第六步,搭建Ribbon+RestTemplate来调用各个微服务

---
+++ gquik-ribbon/src/test/java/com/gqk/gquikribbon/GquikRibbonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gqk.gquikribbon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GquikRibbonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
