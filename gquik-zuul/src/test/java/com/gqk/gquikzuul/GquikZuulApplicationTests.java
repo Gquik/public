019a31bd4c18b3e029da713660bd8f5aadc7f8c7
gequankai <gequankai@zhongye.net>
2020-04-07 16:06:21 +0800

    第七步，搭建zuul路由

---
+++ gquik-zuul/src/test/java/com/gqk/gquikzuul/GquikZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gqk.gquikzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GquikZuulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
