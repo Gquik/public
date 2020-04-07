e8768a62a02181768e03e520956f8549f92d141c
gequankai <gequankai@zhongye.net>
2020-04-07 14:34:07 +0800

    第四步,搭建配置中心,配置地址放在github上

---
+++ gquik-config/src/test/java/com/gqk/gquikconfig/GquikConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gqk.gquikconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GquikConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
